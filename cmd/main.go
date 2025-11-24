6289d17f664593bf919c9d73f102ec47f47b4aff
camward <camward@yandex.ru>
2025-11-24 20:56:17 +0400

    Выделение контроллера

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"go/app/internal/home"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello")
-	})
+	home.NewHandler(app)
 
 	app.Listen(":3000")
 }
