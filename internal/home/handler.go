cabd43e5a81d37699efe201b408ec52189b8bdde
camward <camward@yandex.ru>
2025-11-24 21:18:24 +0400

    Встроенное логирование

--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,6 +1,9 @@
 package home
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+)
 
 type HomeHandler struct {
 	router fiber.Router
@@ -20,5 +23,10 @@ func (h *HomeHandler) home(c *fiber.Ctx) error {
 }
 
 func (h *HomeHandler) error(c *fiber.Ctx) error {
+	log.Info("Info")
+	log.Debug("Debug")
+	log.Warn("Warn")
+	log.Error("Error")
+	log.Panic("Error")
 	return c.SendString("Error")
 }
