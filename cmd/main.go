79b828c6d7faa5537a21c08de4b096876cb0269b
camward <camward@yandex.ru>
2025-11-25 20:38:40 +0400

    Templ. Установка и настройка go install github.com/a-h/templ/cmd/templ@latest go get github.com/a-h/templ templ generate

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,10 @@ import (
 	"go/app/config"
 	"go/app/internal/home"
 	"go/app/pkg/logger"
-	"strings"
 
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -17,16 +15,8 @@ func main() {
 	config.NewDatabaseConfig()
 	logConfig := config.NewLogConfig()
 	customLogger := logger.NewLogger(logConfig)
-	engine := html.New("./html", ".html")
-	engine.AddFuncMap(map[string]interface{}{
-		"ToUpper": func(c string) string {
-			return strings.ToUpper(c)
-		},
-	})
 
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := fiber.New()
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
 	}))
