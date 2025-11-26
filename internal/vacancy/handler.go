88768ee2f109c8047f91319ad4c56e6db4b02220
camward <camward@yandex.ru>
2025-11-26 21:57:16 +0400

    Валидация запроса go get github.com/gobuffalo/validate go get github.com/gobuffalo/validate/validators

--- internal/vacancy/handler.go
+++ internal/vacancy/handler.go
@@ -2,9 +2,12 @@ package vacancy
 
 import (
 	"go/app/pkg/tadapter"
+	"go/app/pkg/validator"
 	"go/app/views/components"
 
 	"github.com/a-h/templ"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
@@ -24,10 +27,15 @@ func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 }
 
 func (h *VacancyHandler) createVacancy(c *fiber.Ctx) error {
-	email := c.FormValue("email")
+	form := VacancyCreateForm{
+		Email: c.FormValue("email"),
+	}
+	errors := validate.Validate(
+		&validators.EmailIsPresent{Name: "Email", Field: form.Email, Message: "Email не задан или неверный"},
+	)
 	var component templ.Component
-	if email == "" {
-		component = components.Notification("Не задан email", components.NotificationFail)
+	if len(errors.Errors) > 0 {
+		component = components.Notification(validator.FormatErrors(errors), components.NotificationFail)
 		return tadapter.Render(c, component)
 	}
 	component = components.Notification("Вакансия успешно создана", components.NotificationSuccess)
