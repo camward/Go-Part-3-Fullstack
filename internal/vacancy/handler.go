54c52acbd6767f27ae417b62a45c949eb2ac6f2b
camward <camward@yandex.ru>
2025-11-26 22:04:24 +0400

    Валидация формы

--- internal/vacancy/handler.go
+++ internal/vacancy/handler.go
@@ -4,7 +4,6 @@ import (
 	"go/app/pkg/tadapter"
 	"go/app/pkg/validator"
 	"go/app/views/components"
-	"time"
 
 	"github.com/a-h/templ"
 	"github.com/gobuffalo/validate"
@@ -29,12 +28,21 @@ func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 
 func (h *VacancyHandler) createVacancy(c *fiber.Ctx) error {
 	form := VacancyCreateForm{
-		Email: c.FormValue("email"),
+		Email:    c.FormValue("email"),
+		Location: c.FormValue("location"),
+		Type:     c.FormValue("type"),
+		Company:  c.FormValue("company"),
+		Role:     c.FormValue("role"),
+		Salary:   c.FormValue("salary"),
 	}
 	errors := validate.Validate(
 		&validators.EmailIsPresent{Name: "Email", Field: form.Email, Message: "Email не задан или неверный"},
+		&validators.StringIsPresent{Name: "Location", Field: form.Location, Message: "Расположение не задано"},
+		&validators.StringIsPresent{Name: "Type", Field: form.Type, Message: "Сфера компании не задана"},
+		&validators.StringIsPresent{Name: "Company", Field: form.Company, Message: "Название компании не задано"},
+		&validators.StringIsPresent{Name: "Role", Field: form.Role, Message: "Должность не задана"},
+		&validators.StringIsPresent{Name: "Salary", Field: form.Salary, Message: "Зарплата не задана"},
 	)
-	time.Sleep(time.Second * 2)
 	var component templ.Component
 	if len(errors.Errors) > 0 {
 		component = components.Notification(validator.FormatErrors(errors), components.NotificationFail)
