cdbd80e60221836185f80bc1d383226576bcddaa
camward <camward@yandex.ru>
2025-11-27 20:58:59 +0400

    Миграции

--- internal/vacancy/repository.go
+++ internal/vacancy/repository.go
@@ -3,6 +3,7 @@ package vacancy
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -22,14 +23,15 @@ func NewVacancyRepository(dbpool *pgxpool.Pool, customLogger *zerolog.Logger) *V
 }
 
 func (r *VacancyRepository) addVacancy(form VacancyCreateForm) error {
-	query := `INSERT INTO vacancies (email, role, company, salary, type, location) VALUES (@email, @role, @company, @salary, @type, @location)`
+	query := `INSERT INTO vacancies (email, role, company, salary, type, location, createdat) VALUES (@email, @role, @company, @salary, @type, @location, @createdat)`
 	args := pgx.NamedArgs{
-		"email":    form.Email,
-		"role":     form.Role,
-		"company":  form.Company,
-		"salary":   form.Salary,
-		"type":     form.Type,
-		"location": form.Location,
+		"email":     form.Email,
+		"role":      form.Role,
+		"company":   form.Company,
+		"salary":    form.Salary,
+		"type":      form.Type,
+		"location":  form.Location,
+		"createdat": time.Now(),
 	}
 	_, err := r.Dbpool.Exec(context.Background(), query, args)
 	if err != nil {
