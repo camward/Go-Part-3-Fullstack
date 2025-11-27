126fa68b593dcbde5c54f3ac57de2398db2add5f
camward <camward@yandex.ru>
2025-11-27 20:45:04 +0400

    Сохранение вакансии

--- internal/vacancy/repository.go
+++ internal/vacancy/repository.go
@@ -1,6 +1,10 @@
 package vacancy
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 )
@@ -17,6 +21,19 @@ func NewVacancyRepository(dbpool *pgxpool.Pool, customLogger *zerolog.Logger) *V
 	}
 }
 
-func (r *VacancyRepository) addVacancy(form VacancyCreateForm) {
-
+func (r *VacancyRepository) addVacancy(form VacancyCreateForm) error {
+	query := `INSERT INTO vacancies (email, role, company, salary, type, location) VALUES (@email, @role, @company, @salary, @type, @location)`
+	args := pgx.NamedArgs{
+		"email":    form.Email,
+		"role":     form.Role,
+		"company":  form.Company,
+		"salary":   form.Salary,
+		"type":     form.Type,
+		"location": form.Location,
+	}
+	_, err := r.Dbpool.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("Невозможно создать вакансию: %w", err)
+	}
+	return nil
 }
